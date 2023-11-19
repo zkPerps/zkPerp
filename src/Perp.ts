f0807b191ac0c380f61204a66fda869117867599
Maga <steam.30mb1@gmail.com>
2023-11-19 04:00:51 +0400

    Multiple positions with merkle map, v2

--- src/Perp.ts
+++ src/Perp.ts
@@ -1,53 +1,124 @@
-import {Bool, Character, Field, Int64, method, Poseidon, Provable, SmartContract, state, State, UInt64} from 'o1js';
+import {
+  Bool,
+  Character,
+  Field,
+  Int64,
+  MerkleMapWitness,
+  method,
+  Poseidon,
+  Provable,
+  SmartContract,
+  state,
+  State,
+  Struct,
+  UInt32,
+  UInt64
+} from 'o1js';
 
 
-export class Perpetual extends SmartContract {
-  @state (Field) position = State<Field>();
-  @state (Bool) closed = State<Bool>();
-  @state (Int64) pnl = State<Int64>();
-
-  @method initState(
+export class Position extends Struct({
     salt: Field,
-    type: Character, // 'l' - long, 's' - short
-    collateral: UInt64,
-    leverage: UInt64,
-    openPrice: UInt64
-  ) {
-    this.position.set(Poseidon.hash([salt, type.value, collateral.value, leverage.value, openPrice.value]));
-  }
-
-  @method closePosition(
-    closePrice: UInt64, // 6 decimals
-    salt: Field,
-    type: Character, // 'l' - long, 's' - short
+    type: Character, // 's' - short, 'l' - long
     collateral: UInt64, // 6 decimals
     leverage: UInt64, // 6 decimals
     openPrice: UInt64 // 6 decimals
-  ) {
-    this.position.assertEquals(this.position.get());
-    this.closed.assertEquals(this.closed.get());
-    this.pnl.assertEquals(this.pnl.get());
-    this.closed.assertEquals(Bool(false));
+    // createdAt: UInt32
+  }) {
+  hash() {
+    return Poseidon.hash([
+      this.salt, // for privacy
+      this.type.value,
+      this.collateral.value,
+      this.leverage.value,
+      this.openPrice.value
+    ]);
+  }
 
-    Poseidon.hash([salt, type.value, collateral.value, leverage.value, openPrice.value]).assertEquals(this.position.get());
-    this.position.set(Poseidon.hash([salt, type.value, collateral.value, leverage.value, openPrice.value, closePrice.value]));
+  closeHash(closePrice: UInt64): Field {
+    return Poseidon.hash([
+      this.salt, // for privacy
+      this.type.value,
+      this.collateral.value,
+      this.leverage.value,
+      this.openPrice.value,
+      closePrice.value
+    ]);
+  }
 
+  calcPnl(closePrice: UInt64): Int64 {
     const SCALING_FACTOR = Int64.from(1_000_000_000).toConstant(); // multiply precision factor
     const LEVERAGE_BASE = UInt64.from(1_000_000).toConstant(); // leverage input decimals
 
-    const leveraged_pos = collateral.mul(leverage).div(LEVERAGE_BASE);
-    let pnl = Provable.if(
-      closePrice.lessThanOrEqual(openPrice),
-      SCALING_FACTOR.sub(Int64.fromUnsigned(closePrice).mul(SCALING_FACTOR).div(openPrice)),
-      Int64.fromUnsigned(closePrice).mul(SCALING_FACTOR).div(openPrice).sub(SCALING_FACTOR)
+    const leveraged_pos = this.collateral.mul(this.leverage).div(LEVERAGE_BASE);
+    let posPnl = Provable.if(
+      closePrice.lessThanOrEqual(this.openPrice),
+      SCALING_FACTOR.sub(Int64.fromUnsigned(closePrice).mul(SCALING_FACTOR).div(this.openPrice)),
+      Int64.fromUnsigned(closePrice).mul(SCALING_FACTOR).div(this.openPrice).sub(SCALING_FACTOR)
     );
-    pnl = Provable.if(
-      type.equals(Character.fromString('s')),
-      pnl.neg(),
-      pnl
+    posPnl = Provable.if(
+      this.type.equals(Character.fromString('s')),
+      posPnl.neg(),
+      posPnl
     );
-    pnl = pnl.mul(leveraged_pos).div(SCALING_FACTOR);
-    this.closed.set(Bool(true));
-    this.pnl.set(pnl);
+    return posPnl.mul(leveraged_pos).div(SCALING_FACTOR);
+  }
+}
+
+export class Perpetual extends SmartContract {
+  @state(Field) positionsMap = State<Field>();
+  @state(UInt64) counter = State<UInt64>();
+  @state(Int64) pnl = State<Int64>();
+
+  @method initState(initialState: Field) {
+    this.positionsMap.set(initialState);
+    this.counter.set(UInt64.from(1));
+    this.pnl.set(Int64.from(0));
+  }
+
+  private _getState(): [Field, UInt64, Int64] {
+    const newPosKey = this.counter.get();
+    this.counter.assertEquals(newPosKey);
+    const root = this.positionsMap.get();
+    this.positionsMap.assertEquals(root);
+    const pnl = this.pnl.get();
+    this.pnl.assertEquals(pnl);
+    return [root, newPosKey, pnl];
+  }
+
+  // @dev Adds new position to positions merkle map using counter as key
+  @method openPosition(
+    keyWitness: MerkleMapWitness,
+    position: Position
+  ) {
+    const [root, newPosKey,] = this._getState();
+
+    // empty leafs have Field(0) as initial value
+    const [rootBefore, key] = keyWitness.computeRootAndKey(Field(0));
+    rootBefore.assertEquals(root);
+    key.assertEquals(newPosKey.value);
+
+    const [ rootAfter, ] = keyWitness.computeRootAndKey(position.hash());
+
+    this.positionsMap.set(rootAfter);
+    this.counter.set(newPosKey.add(1));
+  }
+
+  @method closePosition(
+    keyWitness: MerkleMapWitness,
+    posKey: Field,
+    position: Position,
+    closePrice: UInt64
+  ) {
+    let [root,, pnl] = this._getState();
+
+    // empty leafs have Field(0) as initial value
+    const [rootBefore, key] = keyWitness.computeRootAndKey(position.hash());
+    rootBefore.assertEquals(root);
+    key.assertEquals(posKey);
+
+    const [ rootAfter, ] = keyWitness.computeRootAndKey(position.closeHash(closePrice));
+    this.positionsMap.set(rootAfter);
+
+    this.pnl.set(pnl.add(position.calcPnl(closePrice)));
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 
-import { Perpetual } from './Perp.js';
+import { Perpetual, Position } from './Perp.js';
 import {
   Field,
   Mina,
   PrivateKey,
-  AccountUpdate, Character, UInt64,
+  AccountUpdate, Character, UInt64, Bool, MerkleMap,
 } from 'o1js';
 
 
@@ -17,49 +17,79 @@ const { privateKey: deployerKey, publicKey: deployerAccount } =
 const { privateKey: senderKey, publicKey: senderAccount } =
   Local.testAccounts[1];
 
-const salt = Field.random();
-
 // ----------------------------------------------------
 
 // create a destination we will deploy the smart contract to
 const zkAppPrivateKey = PrivateKey.random();
 const zkAppAddress = zkAppPrivateKey.toPublicKey();
 
+
+const map = new MerkleMap();
+
+
+// --------------------------- DEPLOY --------------------------------
 const zkAppInstance = new Perpetual(zkAppAddress);
 const deployTxn = await Mina.transaction(deployerAccount, () => {
   AccountUpdate.fundNewAccount(deployerAccount);
   zkAppInstance.deploy();
-  zkAppInstance.initState(
-    salt,
-    Character.fromString('s'),
-    UInt64.from(1_000_000),
-    UInt64.from(1_000_000),
-    UInt64.from(1_000_000).mul(2)
-  );
+  zkAppInstance.initState(map.getRoot());
 });
 await deployTxn.prove();
 await deployTxn.sign([deployerKey, zkAppPrivateKey]).send();
 
 // get the initial state of IncrementSecret after deployment
-const pos = zkAppInstance.position.get();
-console.log('state after init:', pos.toString());
+const pos_map = zkAppInstance.positionsMap.get();
+const counter = zkAppInstance.counter.get();
+console.log('state after init:', pos_map.toString(), counter.toString());
 
-// ----------------------------------------------------
-
-const txn1 = await Mina.transaction(senderAccount, () => {
-  zkAppInstance.closePosition(
-      UInt64.from(1_000_000).mul(3),
-      salt,
-      Character.fromString('s'),
-      UInt64.from(1_000_000),
-      UInt64.from(1_000_000),
-      UInt64.from(1_000_000).mul(2)
-    );
+// --------------------------- OPEN POSITION -------------------------------------
+const salt = Field.random();
+const value = new Position({
+  salt: salt,
+  type: Character.fromString('l'), // long
+  collateral: UInt64.from(10_000_000), // 10$
+  leverage: UInt64.from(2_000_000), // 2x
+  openPrice: UInt64.from(30_000_000), // 30k$
+  // createdAt: UInt32
 });
 
-console.log('123');
+const pos_key = counter.value;
+map.set(pos_key, value.hash());
+
+const txn1 = await Mina.transaction(senderAccount, () => {
+  zkAppInstance.openPosition(
+    map.getWitness(pos_key),
+    value
+  );
+});
+
 await txn1.prove();
 await txn1.sign([senderKey]).send();
 
-const closed = zkAppInstance.closed.get();
-console.log('state after txn1:', closed.toString());
+const state_new = zkAppInstance.positionsMap.get();
+const new_counter = zkAppInstance.counter.get();
+console.log('state after txn1:', state_new.toString(), new_counter.toString());
+console.log('local map root', map.getRoot().toString())
+
+// --------------------------- CLOSE POSITION -------------------------------------
+const closePrice = UInt64.from(32_000_000);
+map.set(pos_key, value.closeHash(closePrice));
+const txn2 = await Mina.transaction(senderAccount, () => {
+  zkAppInstance.closePosition(
+    map.getWitness(pos_key),
+    pos_key,
+    value,
+    closePrice
+  );
+});
+
+await txn2.prove();
+await txn2.sign([senderKey]).send();
+
+const state_final = zkAppInstance.positionsMap.get();
+const counter_final = zkAppInstance.counter.get();
+const pnl = zkAppInstance.pnl.get();
+console.log('state after txn2:', state_final.toString(), counter_final.toString());
+console.log('local map root', map.getRoot().toString())
+console.log('pnl', pnl.toString())
+
